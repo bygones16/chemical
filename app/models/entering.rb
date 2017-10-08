01ce13c3cc7ccba767d86c616e9dfd1909340882
jiro.kirameki <rakva492@gmail.com>
2017-10-08 12:42:41 +0000

    chemical

--- app/models/entering.rb
+++ app/models/entering.rb
@@ -1,6 +1,6 @@
 class Entering < ApplicationRecord
-  validates :entering_number, numericality: { only_integer: true, greater_than_or_equal_to: 1 },presence: true, length: { maximum: 50 }
-  validates :entering_person, presence: true, length: { maximum: 50 }
+  validates :entering_number, numericality: { only_integer: true, greater_than_or_equal_to: 1 },presence: true, length: { maximum: 10 }
+  validates :entering_person, presence: true, length: { maximum: 10 }
   belongs_to :chem
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :company_name, presence: true, length: { maximum: 50 }
-  validates :login_id, presence: true, length: { maximum: 50 }
-  validates :password, presence: true, length: { maximum: 50 }
+  validates :company_name, presence: true, length: { maximum: 20 }
+  validates :login_id, presence: true, length: { maximum: 10 }
+  validates :password, presence: true, length: { maximum: 10 }
   has_secure_password
   has_many :chems
 end
