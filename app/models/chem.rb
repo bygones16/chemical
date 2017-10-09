f6ead7be4b44d2fd0b7743d19feda8f39c17520b
jiro.kirameki <rakva492@gmail.com>
2017-10-09 12:21:46 +0000

    chemical

--- app/models/chem.rb
+++ app/models/chem.rb
@@ -1,5 +1,5 @@
 class Chem < ApplicationRecord
-  validates :chemical_name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates :chemical_name, presence: true, length: { maximum: 20}, uniqueness: { case_sensitive: false, scope: :user_id }
 
   has_many :enterings, :dependent => :destroy
   has_many :dispatchings, :dependent => :destroy
